f2b1083c520161d768e54ea207bf700d142bb915
Google61 <golubev634@gmail.com>
2022-02-24 17:15:03 +0300

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -7,7 +7,7 @@ then
   export TARGET_JDK=aarch32
   export TARGET_PHYS=aarch32-linux-androideabi
   export JVM_VARIANTS=client
-  export CFLAGS+=" -march=armv7-a -mfpu=neon-vfpv4"
+  export CFLAGS+=" -march=armv7-a -mfpu=neon-vfpv43"
 else
   export TARGET_PHYS=$TARGET
 fi
