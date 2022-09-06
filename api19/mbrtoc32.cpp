2889f4adf60329e62c3942be27debf1921942e1f
Google61 <golubev634@gmail.com>
2022-09-06 03:47:11 +0300

    Update mbrtoc32.cpp

--- api19/mbrtoc32.cpp
+++ api19/mbrtoc32.cpp
@@ -31,7 +31,7 @@
 #include <uchar.h>
 #include <wchar.h>
 #include <bionic_mbstate.h>
-#include <algorithm>
+//#include <algorithm>
 
 size_t mbrtoc32(char32_t* pc32, const char* s, size_t n, mbstate_t* ps) {
   static mbstate_t __private_state;
